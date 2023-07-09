49bb587040e882ebe455b096f8365a10e58cfad8
amshepard <antoineshepard@pursuit.org>
2023-07-09 16:20:28 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Bookmarks App");
+});
+
+// EXPORT
+module.exports = app;
+
